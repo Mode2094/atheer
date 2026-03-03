2096b6568d7f835e2c4f6aa46c81577633f98ff2
Mode2094 <moderohe8@gmail.com>
2026-03-03 17:44:17 +0300

    Setup GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
-﻿/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/atheer' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/atheer/' : '',
+  trailingSlash: true,
   reactStrictMode: true,
   poweredByHeader: false
 };
